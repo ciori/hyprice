5891fd0a0b7392a867070f84fd9ddcf98ac38971
ciori <mirko@ciori.net>
2023-06-02 23:01:54 +0200

    Update rice

--- README.md
+++ README.md
@@ -2,15 +2,26 @@
 
 Hyprland rice configuration files
 
-## Setup
+
+## Read Before Use
+
+There are some requirements/limitations:
+- **This is created for an AMD Graphics Card equipped system**. The setup will install the package `libva-mesa-driver` used for `wf-recorder`. In case you use something else, look for Hardware/Video Acceleration (VA-API) in the Arch Wiki and change that packet accordingly.
+- **This rice is for a Desktop PC with Ethernet connection and no Bluetooth or WiFi**. The interface lacks all the useful widgets and shortcuts for bluetooth, wifi and battery and has no power management tools. They will probably be added in the future.
+
+## Installation
 
 Steps:
-- Start from a fresh minimal installation of archlinux using the `archinstall` tool
-- Login as you sudo user (non root)
+- start from a fresh installation of archlinux:
+  - use the `archinstall` tool
+  - select a minimal profile
+  - use networkmanager
+  - create a sudo user (non root)
+- login as your non root user
+- ...
 
 ## TODO
 
 List:
-- remove every command interaction
-- pacman conf
-- move packages to separate file
+- remove every command interaction.
+- add laptop support for wifi, bluetooth and battery and power management.
--- hyprice.sh
+++ hyprice.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-#set pacman parallel download and color
-#...
+cd
+
+# pacman configuration
+sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
+sudo sed -i 's/#ParallelDownloads/ParallelDownloads/g' /etc/pacman.conf
+
+# install paru
 pacman -S git rustup
 rustup default stable
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+cd ..
+rm -rf paru
+
+# create home folders
+mkdir -p downloads documents pics/screenshots videos/recordings music
+
+# bash configuration
 paru -S bash-completion
-echo "export HISTCONTROL=ignoreboth" >> ~/.bashrc
-paru -S hyperland foot mako xdg-desktop-portal-hyprland polkit-kde-agent waybar-hyprland-git swaybg
-paru -S terminus-font ttf-jetbrains-mono-nerd
-paru -S pavucontrol
-paru -S vscodium-bin
+grep -qxF 'export HISTCONTROL=ignoreboth' ~/.bashrc || echo "export HISTCONTROL=ignoreboth" >> ~/.bashrc
 
-#copy dotfiles
-#...
+# install packages
+paru -S hyperland xdg-desktop-portal xdg-desktop-portal-hyprland \
+  waybar-hyprland-git swaybg swayidle swaylock bemenu \
+  polkit-kde-agent foot maku pavucontrol htop nm-connection-editor thunar xarchiver \
+  mpv yt-dlp zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps \
+  terminus-font ttf-jetbrains-mono-nerd \
+  greetsd greetd-tuigreet \
+  grim slurp swappy wf-recorder libva-mesa-driver \
+  vscodium-bin thunderbird librewolf torbrowser-launcher
 
-#setup gpg smart card
+# configure greeter
+sudo sed -i 's/user = "greeter"/#user = "greeter"/g' /etc/pacman.conf
+sudo sed -i 's/command = "agreety --cmd /bin/sh"/command = "tuigreet --cmd Hyprland"/g' /etc/pacman.conf
+sudo systemctl enable greetd.service
+
+# copy dotfiles
 #...
